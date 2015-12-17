e3e2bdb5fb3356c67b265abccf607f3c2fbbd5b1
Jakob Metzger <jakob.me@gmail.com>
2015-12-17 19:52:44 +0100

    Erster Commit; alle grundlegenden Dateien und Verzeichnisse (Javascript, CSS, LESS, index.php) hinzugefügt.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+    
+    <!--Head-->
+	<head>
+    	
+    	<!--Meta-->
+		<title>OWLisch</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		
+		<!--CSS-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,400italic,900"  type="text/css">
+        <link rel="stylesheet" href="css/style.min.css?v=1" type="text/css">
+        
+        <!--Javascript-->
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script type="text/javascript" src="js/main.min.js?v=1"></script>
+        
+	</head>
+	
+	<!--Body-->
+	<body>
+	</body>
+	
+</html>
---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+ * Main-Javascript.
+ * @author Jakob Metzger <jakob.me@gmail.com>
+ * @copyright 2015 Jakob Metzger
+ */
+
+/*
+ * Warten, bis das Dokument bereit ist.
+ * Sämtliche Funktionalitäten erst ausführen, sobald das Dokument
+ * vollständig geladen und bereit ist.
+ */
+$(document).ready(function() {
+    
+    // TODO: Alles!
+});

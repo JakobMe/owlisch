604c9b6e70351de8a1411e766a88a7adcb22af42
Jakob Metzger <jakob.me@gmail.com>
2015-12-17 20:25:42 +0100

    FontAwesome integriert, Titel-Leiste hinzugefügt.

--- index.php
+++ index.php
@@ -9,19 +9,48 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
 		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 		
 		<!--CSS-->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,400italic,900"  type="text/css">
-        <link rel="stylesheet" href="css/style.min.css?v=1" type="text/css">
+        <link rel="stylesheet" href="css/style.min.css?v=<?php echo rand(0, 100); ?>" type="text/css">
         
         <!--Javascript-->
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script type="text/javascript" src="js/main.min.js?v=1"></script>
+        <script type="text/javascript" src="js/main.min.js?v=<?php echo rand(0, 100); ?>"></script>
         
 	</head>
 	
 	<!--Body-->
 	<body>
+    	
+    	<!--Top-Leiste-->
+    	<div id="bar-top">
+        	
+        	<!--Statusbar-->
+        	<div id="bar-status"></div>
+        	
+        	<!--Titlebar-->
+        	<div id="bar-title">
+            	
+            	<!--Linker Button-->
+            	<div id="bar-title-left">
+                	<i class="fa fa-chevron-left"></i>
+            	</div>
+            	
+            	<!--Rechter Button-->
+            	<div id="bar-title-right">
+            	</div>
+            	
+            	<!--Titel-->
+            	<div id="bar-title-text">
+                	OWLisch
+            	</div>
+        	
+        	</div>
+        	
+    	</div>
+    	
 	</body>
 	
 </html>

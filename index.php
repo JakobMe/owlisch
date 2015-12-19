2a0a6f05219b5eaa19b47fad75265cd88a0b8438
Jakob Metzger <jakob.me@gmail.com>
2015-12-19 17:55:34 +0100

    Favicons hinzugefügt.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php $v = rand(0, 10000); ?>
 <!DOCTYPE html>
 <html lang="de">
     
@@ -11,13 +12,36 @@
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 		
+		<!--Favicon-->
+		<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png?v=<?php echo $v; ?>">
+        <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png?v=<?php echo $v; ?>">
+        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png?v=<?php echo $v; ?>">
+        <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png?v=<?php echo $v; ?>">
+        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png?v=<?php echo $v; ?>">
+        <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png?v=<?php echo $v; ?>">
+        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png?v=<?php echo $v; ?>">
+        <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png?v=<?php echo $v; ?>">
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png?v=<?php echo $v; ?>">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png?v=<?php echo $v; ?>" sizes="32x32">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-194x194.png?v=<?php echo $v; ?>" sizes="194x194">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png?v=<?php echo $v; ?>" sizes="96x96">
+        <link rel="icon" type="image/png" href="img/favicon/android-chrome-192x192.png?v=<?php echo $v; ?>" sizes="192x192">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png?v=<?php echo $v; ?>" sizes="16x16">
+        <link rel="manifest" href="img/favicon/manifest.json?v=<?php echo $v; ?>">
+        <link rel="shortcut icon" href="img/favicon/favicon.ico?v=<?php echo $v; ?>">
+        <meta name="apple-mobile-web-app-title" content="OWLisch">
+        <meta name="application-name" content="OWLisch">
+        <meta name="msapplication-TileColor" content="#388fab">
+        <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png?v=<?php echo $v; ?>">
+        <meta name="theme-color" content="#388fab">
+		
 		<!--CSS-->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic"  type="text/css">
-        <link rel="stylesheet" href="css/style.min.css?v=<?php echo rand(0, 1000); ?>" type="text/css">
+        <link rel="stylesheet" href="css/style.min.css?v=<?php echo $v; ?>" type="text/css">
         
         <!--Javascript-->
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script type="text/javascript" src="js/main.min.js?v=<?php echo rand(0, 1000); ?>"></script>
+        <script type="text/javascript" src="js/main.min.js?v=<?php echo $v; ?>"></script>
         
 	</head>
 	

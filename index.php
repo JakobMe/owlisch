9cd1c5f36f15c9fc586f25ba89a6b04d4d732c74
Jakob Metzger <jakob.me@gmail.com>
2016-01-11 13:42:58 +0100

    Head der index.php aufgeräumt und übersichtlicher formatiert.

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php $v = rand(0, 10000); ?>
+<?php
+    
+    // Einstellungen
+    $v       = rand(0, 10000);
+    $app     = "OWLisch";
+    $color   = "#3bbb77";
+    $favicon = "img/favicon";
+    
+?>
 <!DOCTYPE html>
 <html lang="de">
     
@@ -6,34 +14,42 @@
     <head>
         
         <!--Meta-->
-        <title>OWLisch</title>
+        <title><?php echo $app; ?></title>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         
-        <!--Favicon-->
-        <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-194x194.png" sizes="194x194">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png" sizes="96x96">
-        <link rel="icon" type="image/png" href="img/favicon/android-chrome-192x192.png" sizes="192x192">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png" sizes="16x16">
-        <link rel="manifest" href="img/favicon/manifest.json">
-        <link rel="shortcut icon" href="img/favicon/favicon.ico">
-        <meta name="apple-mobile-web-app-title" content="OWLisch">
-        <meta name="application-name" content="OWLisch">
-        <meta name="msapplication-TileColor" content="#3bbb77">
-        <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png">
-        <meta name="theme-color" content="#3bbb77">
+        <!--Favicons: Apple-->
+        <link rel="apple-touch-icon" type="image/png" sizes="57x57"   href="<?php echo $favicon; ?>/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="60x60"   href="<?php echo $favicon; ?>/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="72x72"   href="<?php echo $favicon; ?>/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="76x76"   href="<?php echo $favicon; ?>/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="114x114" href="<?php echo $favicon; ?>/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="120x120" href="<?php echo $favicon; ?>/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="144x144" href="<?php echo $favicon; ?>/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="152x152" href="<?php echo $favicon; ?>/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="<?php echo $favicon; ?>/apple-touch-icon-180x180.png">
+        
+        <!--Favicons: Android-->
+        <link rel="icon"             type="image/png" sizes="16x16"   href="<?php echo $favicon; ?>/favicon-16x16.png">
+        <link rel="icon"             type="image/png" sizes="32x32"   href="<?php echo $favicon; ?>/favicon-32x32.png">
+        <link rel="icon"             type="image/png" sizes="96x96"   href="<?php echo $favicon; ?>/favicon-96x96.png">
+        <link rel="icon"             type="image/png" sizes="192x192" href="<?php echo $favicon; ?>/android-chrome-192x192.png">
+        <link rel="icon"             type="image/png" sizes="194x194" href="<?php echo $favicon; ?>/favicon-194x194.png">
+        <link rel="manifest"                                          href="<?php echo $favicon; ?>/manifest.json">
+        
+        <!--Favicons: Desktop-->
+        <link rel="shortcut icon" href="<?php echo $favicon; ?>/favicon.ico">
+        
+        <!--Webapp-Einstellungen-->
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <meta name="apple-mobile-web-app-capable"          content="yes">
+        <meta name="apple-mobile-web-app-title"            content="<?php echo $app; ?>">
+        <meta name="application-name"                      content="<?php echo $app; ?>">
+        <meta name="theme-color"                           content="<?php echo $color; ?>">
+        <meta name="msapplication-TileColor"               content="<?php echo $color; ?>">
+        <meta name="msapplication-TileImage"               content="<?php echo $favicon; ?>/mstile-144x144.png">
+        <meta name="viewport"                              content="width=device-width, initial-scale=1,
+                                                                    minimum-scale=1, maximum-scale=1,
+                                                                    user-scalable=0">
         
     </head>
     

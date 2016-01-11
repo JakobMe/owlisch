07123e9cf647cc25f3c111b1be8bf49654d5fe2f
Jakob Metzger <jakob.me@gmail.com>
2016-01-11 13:31:18 +0100

    Versionen von Favicons entfernt (Favicons sind jetzt final).

--- index.php
+++ index.php
@@ -13,26 +13,26 @@
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         
         <!--Favicon-->
-        <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png?v=<?php echo $v; ?>">
-        <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png?v=<?php echo $v; ?>">
-        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png?v=<?php echo $v; ?>">
-        <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png?v=<?php echo $v; ?>">
-        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png?v=<?php echo $v; ?>">
-        <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png?v=<?php echo $v; ?>">
-        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png?v=<?php echo $v; ?>">
-        <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png?v=<?php echo $v; ?>">
-        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png?v=<?php echo $v; ?>">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png?v=<?php echo $v; ?>" sizes="32x32">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-194x194.png?v=<?php echo $v; ?>" sizes="194x194">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png?v=<?php echo $v; ?>" sizes="96x96">
-        <link rel="icon" type="image/png" href="img/favicon/android-chrome-192x192.png?v=<?php echo $v; ?>" sizes="192x192">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png?v=<?php echo $v; ?>" sizes="16x16">
-        <link rel="manifest" href="img/favicon/manifest.json?v=<?php echo $v; ?>">
-        <link rel="shortcut icon" href="img/favicon/favicon.ico?v=<?php echo $v; ?>">
+        <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-194x194.png" sizes="194x194">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="img/favicon/android-chrome-192x192.png" sizes="192x192">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="img/favicon/manifest.json">
+        <link rel="shortcut icon" href="img/favicon/favicon.ico">
         <meta name="apple-mobile-web-app-title" content="OWLisch">
         <meta name="application-name" content="OWLisch">
         <meta name="msapplication-TileColor" content="#3bbb77">
-        <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png?v=<?php echo $v; ?>">
+        <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png">
         <meta name="theme-color" content="#3bbb77">
         
     </head>

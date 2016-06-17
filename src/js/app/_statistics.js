e150ab18c239fc39142f7a7ecc7d7f7abb138995
Jakob Metzger <jakob.me@gmail.com>
2016-06-17 02:08:56 +0200

    Weitere Diagramme in Statistik gestaltet und implementiert.

--- src/js/app/_statistics.js
+++ src/js/app/_statistics.js
@@ -16,9 +16,14 @@ var Statistics = (function() {
     var _SEL_TMPL_STATISTICS    = "#tmpl-statistics";
     var _SEL_TMPL_CHART         = "#tmpl-chart";
     
+    // Sonstige Konstanten
+    var _NUM_STEPS_PERCENT      = 10;
+    
     // Private Variablen
     var _listLastgames          = [];
     var _listProgress           = [];
+    var _arrStepsPercent        = [];
+    var _arrStepsLastgames      = [];
     var _sizeProgress           = 0;
     var _sizeDictionary         = 0;
     
@@ -36,6 +41,12 @@ var Statistics = (function() {
      * Startet Funktionen, um den Anfangszustand der Statistik herzustellen.
      */
     function init() {
+        
+        // Interne Variablen initialisieren
+        _arrStepsPercent   = _arrayFromNumber(_NUM_STEPS_PERCENT);
+        _arrStepsLastgames = _arrayFromNumber(CFG.QUIZ.NUM_STEPS);
+        
+        // Funktionen ausführen
         _parseTemplates();
         _bindEvents();
     }
@@ -128,6 +139,7 @@ var Statistics = (function() {
             _sizeProgress   = data.size;
             _sizeDictionary = data.max;
             _renderProgress();
+            _renderDictionary();
         }
     }
     
@@ -144,18 +156,72 @@ var Statistics = (function() {
     }
     
     /**
+     * Prozentwert zweier Zahlen berechnen.
+     * Berechnet den prozentualen Anteil einer Zahl an einer anderen;
+     * gibt das Ergebnis in Prozentpunkten zurück.
+     * @param {Number} first Erster Zahlenwert
+     * @param {Number} second Zweiter Zahlenwert
+     * @returns {Number} Prozentpunkte
+     */
+    function _calcPercent(first, second) {
+        return Math.round((first / second) * 100);
+    }
+    
+    /**
+     * Begriffe mit Level zählen.
+     * Zählt in einer gegeben Begriff-Liste alle Einträge,
+     * die das angegebene Level haben.
+     * @param {Object[]} list Begriff-Liste
+     * @param {Number} level Gesuchter Level
+     * @returns {Number} Anzahl der gefundenen Begriffe
+     */
+    function _countTermsWithLevel(list, level) {
+        var count = 0;
+        $.each(list, function(i, data) {
+            if (data.lvl === level) { count++; }
+        });
+        return count;
+    }
+    
+    /**
+     * Diagramm rendern.
+     * Rendert ein Diagramm mit gegebenen Eigenschaften anhand eines
+     * Mustache-Templates in einen angegebenen Container.
+     * @param {Object} $target Ziel-DOM-Element
+     * @param {Object[]} data Array der Diagramm-Daten
+     * @param {Number[]} steps Array der Skala-Schritte
+     * @param {Boolean} hrznt Horizontales Diagramm
+     * @param {Boolean} stars Level-Sterne anzeigen
+     */
+    function _renderChart($target, data, steps, hrznt, stars) {
+        if (typeof hrznt !== typeof true) { hrznt = false; }
+        if (typeof stars !== typeof true) { stars = false; }
+        $target.find(_SEL_CHART).html(
+            Mustache.render(_tmplChart, {
+                data  : data,
+                hrznt : hrznt,
+                steps : steps,
+                stars : stars
+            })
+        );
+    }
+    
+    /**
      * Diagramm für letzte Spielergebnisse rendern.
      * Rendert ein Diagramm für die letzten Spielergebnisse
      * anhand eines Mustache-Templates.
      */
     function _renderLastgames() {
         if (_$lastgames instanceof $) {
-            _$lastgames.find(_SEL_CHART).html(
-                Mustache.render(_tmplChart, {
-                    steps : _arrayFromNumber(CFG.QUIZ.NUM_STEPS),
-                    data  : _listLastgames
-                })
-            );
+            var data = [];
+            $.each(_listLastgames, function(i, value) {
+                data.push({
+                    label   : value,
+                    percent : _calcPercent(value, CFG.QUIZ.NUM_STEPS),
+                    zero    : (value === 0)
+                });
+            });
+            _renderChart(_$lastgames, data, _arrStepsLastgames);
         }
     }
     
@@ -165,8 +231,34 @@ var Statistics = (function() {
      * anhand eines Mustache-Templates.
      */
     function _renderProgress() {
-        
-        // !TODO _renderProgress()
+        if (_$progress instanceof $) {
+            var data = [{
+                label   : _sizeProgress + CFG.STR.SLASH + _sizeDictionary,
+                percent : _calcPercent(_sizeProgress, _sizeDictionary)
+            }];
+            _renderChart(_$progress, data, _arrStepsPercent, true);
+        }
+    }
+    
+    /**
+     * Diagramme für Wörterbuch rendern.
+     * Rendert die Diagramme für das Wörterbuch
+     * anhand eines Mustache-Templates.
+     */
+    function _renderDictionary() {
+        if (_$dictionary instanceof $) {
+            var data = [];
+            $.each(CFG.QUIZ.LEVELS, function(i, level) {
+                var count = _countTermsWithLevel(_listProgress, level);
+                data.push({
+                    lvl     : level,
+                    levels  : CFG.QUIZ.LEVELS,
+                    label   : count,
+                    percent : _calcPercent(count, _sizeProgress)
+                });
+            });
+            _renderChart(_$dictionary, data, _arrStepsPercent, true, true);
+        }
     }
     
     // Öffentliches Interface
